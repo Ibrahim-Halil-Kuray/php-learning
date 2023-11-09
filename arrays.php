a3c5bd679c3023c7840010461a036a9bfe364066
Ibrahim-Halil-Kuray <71791007+Ibrahim-Halil-Kuray@users.noreply.github.com>
2023-11-09 03:21:42 +0100

    Create arrays.php

---
+++ arrays.php
@@ -0,0 +1,15 @@
+<?php
+
+    // Arrays
+        // Numeric
+        // Associative
+
+
+    // $students = "A, B, C";
+    // $students = array("A", "B", "C");
+
+    $students = ["A", "B", "C"];
+
+    echo $students[0] // A
+
+?>
