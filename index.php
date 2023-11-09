9ddb8758b069d0bfaeca663fae7f428dc0999655
Ibrahim-Halil-Kuray <71791007+Ibrahim-Halil-Kuray@users.noreply.github.com>
2023-11-09 01:52:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
+
+<?php
+$title = "Hi there, this is my php learning journey!";
+?>
+
 <html>
     <head>
         <title>PHP Test</title>
     </head>
     <body>
+        <?php echo $title; ?>
         <?php echo '<h1>Hello World</h1>'; ?>
     </body>
 </html>
