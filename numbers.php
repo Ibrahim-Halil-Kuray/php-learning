6fb32da8f7b02bb0129d2e783b932f14d85c30d7
Ibrahim-Halil-Kuray <71791007+Ibrahim-Halil-Kuray@users.noreply.github.com>
2023-11-09 03:15:41 +0100

    Create numbers.php

---
+++ numbers.php
@@ -0,0 +1,27 @@
+<?php
+
+    $number1 = 12;
+    $number2 = 18;
+    $number3 = 1.1;
+
+    echo "Result : ".($number1 + $number2)."<br>"; // 30
+    echo "Result : ".($number1 - $number2)."<br>"; // -6
+    echo "Result : ".($number1 * $number2)."<br>"; // 216
+    echo "Result : ".($number1 / $number2)."<br>"; // 0.66
+
+    echo is_int($number1)."<br>"; // 1 (true)
+
+    echo is_int($number3)."<br>"; // 0 (false but result is not visible) double
+
+    echo var_dump(is_int($number3))."<br>"; // bool(false) variable type and the value.
+
+    echo ceil(4.1)."<br>"; // 5
+    echo ceil(4.9)."<br>"; // 5
+
+    echo floor(4.9)."<br>"; // 4
+    echo floor(4.1)."<br>"; // 4
+
+    echo round(4.4)."<br>"; // 4
+    echo round(4.6)."<br>"; // 5
+
+?>
