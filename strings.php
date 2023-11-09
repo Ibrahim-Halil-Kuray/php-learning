0204a6532e095662b7f63d4cea2849efa5b602a5
Ibrahim-Halil-Kuray <71791007+Ibrahim-Halil-Kuray@users.noreply.github.com>
2023-11-09 03:02:04 +0100

    Update strings.php

--- strings.php
+++ strings.php
@@ -6,17 +6,33 @@
     $taxP = 0.07;
     $totalTax = $price * $taxP; 
 
-    $result = $item."`s price is ".$price." ".$currency.".";
+    //$result = $item."`s price is ".$price." ".$currency.".";
     
-    $result2 = "{$item}`s price is {$price} {$currency}.";
+    $result = "{$item}`s price is {$price} {$currency}.";
+
+    echo "<br>";
 
     echo $result;
 
     echo "<br>";
 
-    echo $result2;
+    echo "Tax : ".$totalTax;
 
     echo "<br>";
 
-    echo "Tax : ".$totalTax;
+    echo $result[4]; // n
+    echo "<br>";
+    
+    echo strlen($result); // 30 char len str
+    echo "<br>";
+
+    echo str_word_count($result); // 5 word
+    echo "<br>";
+
+    echo strtolower($result); // lower case
+    echo "<br>";
+
+    echo strtoupper($result); // upper case
+    echo "<br>";
+
 ?>
