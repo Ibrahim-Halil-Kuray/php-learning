4d8931373e6721423be5f1ecc311fbd6fa912498
Ibrahim-Halil-Kuray <71791007+Ibrahim-Halil-Kuray@users.noreply.github.com>
2023-11-09 02:20:14 +0100

    Create strings.php

---
+++ strings.php
@@ -0,0 +1,11 @@
+<?php
+
+    $item = "Iphone 14";
+    $price = "1000";
+    $currency = "usd";
+    $taxP = 0.07;
+
+    $result = $item."`s price is ".$price." ".$currency.".";
+
+    echo $result;
+?>
