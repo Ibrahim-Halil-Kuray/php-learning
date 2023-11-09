d28b6f9143133a14243fc7e0005398be3e12d9c0
Ibrahim-Halil-Kuray <71791007+Ibrahim-Halil-Kuray@users.noreply.github.com>
2023-11-09 02:47:16 +0100

    Update strings.php

--- strings.php
+++ strings.php
@@ -4,8 +4,19 @@
     $price = "1000";
     $currency = "usd";
     $taxP = 0.07;
+    $totalTax = $price * $taxP; 
 
     $result = $item."`s price is ".$price." ".$currency.".";
+    
+    $result2 = "{$item}`s price is {$price} {$currency}.";
 
     echo $result;
+
+    echo "<br>";
+
+    echo $result2;
+
+    echo "<br>";
+
+    echo "Tax : ".$totalTax;
 ?>
