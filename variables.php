659d9ad1da8334bf2ad0c0679b4daafae40dbcf1
Ibrahim-Halil-Kuray <71791007+Ibrahim-Halil-Kuray@users.noreply.github.com>
2023-11-09 02:13:32 +0100

    Update variables.php

--- variables.php
+++ variables.php
@@ -22,6 +22,16 @@
 
     echo $number1 + $number2;
 
-
     
+    //   PHP data types
+
+        // string   => "some text"
+        // int      => 1,2,3,4
+        // double   => 10.2, 1.3
+        // boolean  => treu, false
+        // object
+        // array
+        // null
+    
+
 ?>
